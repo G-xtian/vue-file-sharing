750498f7ca591d198c13471d2f2e9dfa27253ac3
G-xtian <g17352542010@163.com>
2023-03-03 10:23:06 +0800

    2023/3/1 第一次测试提交

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,7 +8,16 @@
             </el-header>
             <el-container>
                 <el-aside width="200px">Aside</el-aside>
-                <el-main>Main</el-main>
+                <el-main>
+                    <el-row>
+                        <el-button>默认按钮</el-button>
+                        <el-button type="primary">主要按钮</el-button>
+                        <el-button type="success">成功按钮</el-button>
+                        <el-button type="info">信息按钮</el-button>
+                        <el-button type="warning">警告按钮</el-button>
+                        <el-button type="danger">危险按钮</el-button>
+                    </el-row>
+                </el-main>
             </el-container>
         </el-container>
     </div>
